2314de6105009a3f6ec6f74d648ae4696b99ae5d
Erol Iscioglu <110387801+erolcum@users.noreply.github.com>
2023-10-04 15:09:15 +0300

    Add files via upload

---
+++ Program-2.cs
@@ -0,0 +1,44 @@
+﻿
+using System.Diagnostics;
+using System.IO;
+using static Program;
+
+class Program
+{
+    public static void Main()
+    {
+        Man henry = new Man();
+        Hombre enrique = new Hombre();
+
+        henry.speak();
+        henry.speak("It’s a beautiful evening"	);
+
+        enrique.speak("Hola...");
+        ((Man)enrique).speak("Ne diyon kardeşim"); // explicit cast
+
+        Console.ReadKey();
+    }
+
+    public class Man
+    {
+        public void speak()
+        {
+            Console.Write("Hello: ");
+        }
+        public void speak(string message)
+        {
+            Console.WriteLine(message + "!\n"	);
+        }
+    }
+
+    public class Hombre : Man
+    {
+        public new void speak(string message) // hide base method with new
+        {
+            base.speak(); // call base method in Man
+            Console.WriteLine(message);
+        }
+    }
+
+
+}
